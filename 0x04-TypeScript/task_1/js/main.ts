91c9e2482c97aa8b52b0a5727a7229af7781fc48
Ashraf Magdy Mostafa <ashrafmagdy926@gmail.com>
2025-07-12 23:35:22 +0300

    updated directories-  extended teacher by Directors

---
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -0,0 +1,37 @@
+//Teacher interface with readonly properties and optional attributes
+interface Teacher {
+    readonly firstName: string;
+    readonly lastName: string;
+    fullTimeEmployee: boolean;
+    yearsOfExperience?: number;
+    location: string;
+    [attribute: string]: any;
+}
+
+// Director interface extends Teacher
+interface Directors extends Teacher {
+    numberOfReports: number;
+}
+
+
+
+// Example usage of the Teacher interface
+const teacher3: Teacher = {
+    firstName: 'John',
+    fullTimeEmployee: false,
+    lastName: 'Doe',
+    location: 'London',
+    contract: false,
+};
+
+console.log(teacher3);
+
+// Example usage of the Directors interface
+const director1: Directors = {
+    firstName: 'John',
+    lastName: 'Doe',
+    location: 'London',
+    fullTimeEmployee: true,
+    numberOfReports: 17,
+};
+console.log(director1);
